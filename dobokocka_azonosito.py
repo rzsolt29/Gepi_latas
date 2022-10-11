4217e6d23b14a67a12e43e7a950fee0c431086d5
Révész Zsolt <revzsolt29@gmail.com>
2022-10-11 15:25:06 +0200

    Korok detektalasa

--- dobokocka_azonosito.py
+++ dobokocka_azonosito.py
@@ -1,50 +1,71 @@
-import cv2 as cv
+import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 #kép importálás és átméretezés
 
-img = cv.imread("src/01.jpg")
-#print('Original Picture Dimensions : ',img.shape)
- 
-scale_percent = 20
-width = int(img.shape[1] * scale_percent / 100)
-height = int(img.shape[0] * scale_percent / 100)
-dim = (width, height)
-resized=cv.resize(img,dim)
-#print('Resized Picture Dimensions : ',resized.shape)
-#cv.imshow("Resized image", resized)
+img = cv2.imread("src/2.jpg",0)
+
+x = 650 / img.shape[0]
+y = x
+img = cv2.resize(img, None, None, x, y, cv2.INTER_CUBIC)
+
+
 
 
 #kép szürkeárnyalatossá alakítása
 
-gray = cv.cvtColor(resized, cv.COLOR_BGR2GRAY)
-cv.imshow("Grey image", gray)
+#gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)       #HoughCircles miatt kimarad, mert a HoughCircles végtelen ciklusba esne
+#cv2.imshow("Grey image", gray)
 
 
 #simítás
 
-#gauss3=cv.GaussianBlur(gray,(3,3),0)
-#cv.imshow("Blured image 3", gauss3)
+#gauss3=cv2.GaussianBlur(gray,(3,3),0)
+#cv2.imshow("Blured image 3", gauss3)
 
-#gauss5=cv.GaussianBlur(gray,(5,5),0)
-#cv.imshow("Blured image 5", gauss5)
+gauss5=cv2.GaussianBlur(img,(5,5),0)
+#cv2.imshow("Blured image 5", gauss5)
 
-#bilateral = cv.bilateralFilter(gray,9,75,75)           #a kockán lévő pöttyök nem elég homogének
-#cv.imshow("Bilateral filterde image",bilateral)
+bilateral = cv2.bilateralFilter(img,9,75,75)           #
+#cv2.imshow("Bilateral filterde image",bilateral)
 
-median = cv.medianBlur(gray,5)                          #ez a simítás tűnik a legjobbank
-#cv.imshow("Median blured image",median)
+median = cv2.medianBlur(img,5)                          #ez a simítás tűnik a legjobbank, de a HoughCircles mégias a bilateralt jobban kezeli
+#cv2.imshow("Median blured image",median)
+
+
+#morphologyEx 
+k = np.ones((3, 3))
+morph = cv2.morphologyEx(median, cv2.MORPH_CLOSE, k)
+
+#cv2.imshow("morph image", morph)
 
 # Canny éldetektor
 
-edges = cv.Canny(median,100,200)                        #Gauss-nál kevésbé zajos
-cv.imshow("Canny Edge Detection median",edges)
-#edges2 = cv.Canny(gauss5,100,200)
-#cv.imshow("Canny Edge Detection gauss5",edges2)
+edges = cv2.Canny(morph,100,200)                        #Gauss-nál kevésbé zajos
+#cv2.imshow("Canny Edge Detection median",edges)
+edges2 = cv2.Canny(gauss5,100,200)
+#cv2.imshow("Canny Edge Detection gauss5",edges2)
+edges3 = cv2.Canny(bilateral,100,200)
+#cv2.imshow("Canny Edge Detection gauss5",edges2)
 
 
+cimg = cv2.cvtColor(edges3,cv2.COLOR_GRAY2BGR)
+
+rows = median.shape[0]
+circles = cv2.HoughCircles(edges3, cv2.HOUGH_GRADIENT, 1, rows / 100,
+                            param1=240, param2=23,
+                            minRadius=1, maxRadius=50)
+
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
+cv2.imshow('detected circles',cimg)
 
 
-
-cv.waitKey(0)
-cv.destroyAllWindows()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
