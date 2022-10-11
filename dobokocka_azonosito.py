03f5c63d3ed8a2685e2c6ec3d3e72ae6e56bb827
Révész Zsolt <revzsolt29@gmail.com>
2022-10-11 16:11:07 +0200

    HoughCircles parameter valtoztatas

--- dobokocka_azonosito.py
+++ dobokocka_azonosito.py
@@ -4,13 +4,11 @@ import matplotlib.pyplot as plt
 
 #kép importálás és átméretezés
 
-img = cv2.imread("src/kozeli/10.jpg",0)
+img = cv2.imread("src/kozeli/13.jpg",0)
 
 x = 650 / img.shape[0]
 y = x
 img = cv2.resize(img, None, None, x, y, cv2.INTER_CUBIC)
-
-
 
 
 #kép szürkeárnyalatossá alakítása
@@ -37,12 +35,12 @@ median = cv2.medianBlur(img,5)                          #ez a simítás tűnik a
 #morphologyEx 
 k = np.ones((3, 3))
 morph = cv2.morphologyEx(median, cv2.MORPH_CLOSE, k)
-
 #cv2.imshow("morph image", morph)
+
 
 # Canny éldetektor
 
-edges = cv2.Canny(morph,100,200)                        #Gauss-nál kevésbé zajos
+edges = cv2.Canny(morph,100,200)
 #cv2.imshow("Canny Edge Detection median",edges)
 edges2 = cv2.Canny(gauss5,100,200)
 #cv2.imshow("Canny Edge Detection gauss5",edges2)
@@ -52,19 +50,24 @@ edges3 = cv2.Canny(bilateral,90,190)
 
 cimg = cv2.cvtColor(edges3,cv2.COLOR_GRAY2BGR)
 
-rows = median.shape[0]
+rows = edges3.shape[0]
 circles = cv2.HoughCircles(edges3, cv2.HOUGH_GRADIENT, 1, rows / 100,
                             param1=240, param2=23,
-                            minRadius=1, maxRadius=50)
+                            minRadius=1, maxRadius=90)                      #max=90 eddig a legjobb
 
 circles = np.uint16(np.around(circles))
+points=0
 for i in circles[0,:]:
     # draw the outer circle
     cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
     # draw the center of the circle
     cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+    points+=1
+
 
 cv2.imshow('detected circles',cimg)
+print("Dobott pontok:")
+print(points)
 
 
 cv2.waitKey(0)
